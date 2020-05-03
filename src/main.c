458f965ef680bbb99f96f98425aa3b595a9676ed
James Duffy <james@duffy.app>
2020-05-03 17:59:00 +0100

    Added Menu
    Added basic menu functionality..

--- src/main.c
+++ src/main.c
@@ -8,8 +8,35 @@
 #include "header.h"
 #include "ver.h"
 
+const char *menuItems[] = {"Prerequesites",
+                         "Option 2",
+                         "Option 3",
+                         "Option 4"};
+
 int main() {
     printmenu();
+    printf("\nMain Menu:\n");
+    printf("");
+    for (int i = 0; i < ARRAY_SIZE(menuItems); i++) {
+        printf("%d: %s\n", i+1, menuItems[i]);
+    }
+    char *optchar;
+    printf("> ");
+    fgets(&optchar, 50, stdin);
+    int opt = atoi(&optchar)-1;
+    if (opt < ARRAY_SIZE(menuItems)){
+        //execute opt
+        switch(opt){
+            case 1:
+                installer();
+                break;
+            default:
+                printf("Option Invalid");
+        }
+    } else{
+        printf("Invalid ID\n");
+        exit(1);
+    }
     //installer();
     return 0;
 }

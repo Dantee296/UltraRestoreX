984554ed4fc4b30b16a7b746f1b0744147569aa5
James Duffy <james@duffy.app>
2020-05-10 14:42:12 +0100

    Dependency Automation

--- src/installer.c
+++ src/installer.c
@@ -8,20 +8,34 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+//need to move the order around ....
+const char *appName[] = {"XCode CLI Tools", //working verified
+                         "Brew", //needs fix
+                         "LibUSB", //working verified
+                         "SSHPass", //working verified
+                         "iPhone Tunnel", //working verified , need manual setup
+                         "libimobiledevice", //working verified
+                         "ideviceinstaller", //working verified
+                         "img4", //working
+                         "img4tool", //working, installs bin
+                         "Kairos", //working
+                         "iBoot64Patcher", //working, installs bin
+                         "liboffsetfinder64" //working, installs bin
+                        };
 
-const char *appName[] = {"Brew",
-                         "LibUSB",
-                         "SSHPass",
-                         "iPhone Tunnel",
-                         "libimobiledevice",
-                         "ideviceinstaller"};
-
-const char *installComm[] = {"echo Install from Brew.SH (this opt has an issue sorry! progress with\nthe rest of the prereqs\nAfter installing from https://brew.sh)",
+const char *installComm[] = {"sudo xcode-select --install",
+                             "echo Install from Brew.SH (this opt has an issue sorry! progress with\nthe rest of the prereqs\nAfter installing from https://brew.sh)",
                              "brew install libusb",
                              "brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb",
                              "curl -o iPhoneTunnel.zip https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/iphonetunnel-mac/iPhoneTunnel2.3-beta1.zip",
                              "brew install libimobiledevice",
-                             "brew install ideviceinstaller"};
+                             "brew install ideviceinstaller",
+                             //add silencing after full tests...
+                             "git clone https://github.com/xerub/img4lib.git && cd img4lib && git submodule init && git submodule update && make -C lzfse && make && mv img4 ../img4",
+                             "git clone https://github.com/tihmstar/img4tool.git && cd img4tool && git submodule init && git submodule update && ./autogen.sh && make && sudo make install",
+                             "git clone https://github.com/dayt0n/kairos.git && cd kairos && make && cd ../ && mv kairos/kairos iboot13patcher",
+                             "git clone https://github.com/tihmstar/iBoot64Patcher.git && cd iboot64patcher && ./autogen.sh && make && make install",
+                             "git clone https://github.com/Ralph0045/liboffsetfinder64.git && cd liboffsetfinder64 && ./autogen.sh && make && make install"};
 
 //takes install arguement with App Name
 install(const char *install) {
